2dd3648715688d41a0d90bc435946f2f824fcf2c
MINHNHAT <nhathoangminhpham@gmail.com>
2017-04-22 15:07:24 +0700

    add base url

--- Gruntfile.js
+++ Gruntfile.js
@@ -109,7 +109,6 @@ module.exports = function(grunt) {
                 }
             }
         },
-
         // Make sure code styles are up to par and there are no obvious mistakes
         jshint: {
             options: {
