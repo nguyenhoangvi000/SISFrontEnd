0afbe4ce7ba0a1973de8caa24c76ed5fba47fb36
nguyenvietca <nguyenvietca199x@gmail.com>
2017-04-28 00:02:35 +0700

    add course courseType

--- app/scripts/services/appService.js
+++ app/scripts/services/appService.js
@@ -7,6 +7,6 @@
             var protocol = 'http';
             var host = 'localhost';
             var port = '8081';
-            this.baseUrl = `${protocol}://${host}:${port}/isc-stuinfo` // http://localhost:8080  
+            this.baseUrl = `${protocol}://${host}:${port}/stuinfo` // http://localhost:8080  
         })
 }());
